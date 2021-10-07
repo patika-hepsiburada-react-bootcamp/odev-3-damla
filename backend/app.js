f4ba6c49dc7ce4a7dad0c47fcab186fe19b8ecb9
Damla Koksal <me@damlakoksal.com>
2021-10-08 02:41:37 +0300

    [#6] feat: fundamental imports done

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.end("realtime voting app");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => console.log("a user disconnected"));
+});
+
+httpServer.listen(3000, () => {
+  console.log("listening on *:3000");
 });
