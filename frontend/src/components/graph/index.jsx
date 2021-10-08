03321e2d98e9f3b2e4ca218a3059ce8ca7a0963a
Damla Koksal <me@damlakoksal.com>
2021-10-08 15:24:54 +0300

    [#2] feat: add graph component

--- frontend/src/components/graph/index.jsx
+++ frontend/src/components/graph/index.jsx
@@ -1,7 +1,34 @@
-// import styles from "./graph.module.scss"
+// import styles from "./graph.module.scss";
+import Chart from "react-apexcharts";
+
+const data = {
+  options: {
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: ["A", "B", "C", "D", "other"],
+    },
+  },
+  series: [
+    {
+      name: "Pool Results",
+      data: [30, 30, 45, 50, 49],
+    },
+  ],
+};
 
 function Graph() {
-  return <div>Graph</div>;
+  return (
+    <div>
+      <Chart
+        options={data.options}
+        series={data.series}
+        type="bar"
+        width="500"
+      />
+    </div>
+  );
 }
 
 export default Graph;
